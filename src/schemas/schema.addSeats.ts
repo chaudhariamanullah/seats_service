7ea3da7d9ac1045df4b1b6dd06de47c45420ac61
chaudhariamanullah <chaudhari.amanullah0@gmail.com>
2026-04-11 09:12:13 +0530

    Limit total_seats to a maximum of 100

--- src/schemas/schema.addSeats.ts
+++ src/schemas/schema.addSeats.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const AddSeatsSchema = z.object({
-    total_seats:z.coerce.number().max(500),
+    total_seats:z.coerce.number().max(100),
     showtime_public_id: z.string()
 }).strict();
 
